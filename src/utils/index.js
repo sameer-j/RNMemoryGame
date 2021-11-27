795b6b9578ac02f62e40f4210a0701c7e3351404
Sameer Jain <samir4nov@gmail.com>
2021-11-28 02:41:45 +0530

    update shuffle function to pure function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,8 @@
-export const shuffle = arr => {
+export const shuffle = array => {
+  let arr = [...array];
   for (let current = arr.length - 1; current > 0; current--) {
     const random = Math.floor(Math.random() * current);
     [arr[current], arr[random]] = [arr[random], arr[current]];
   }
+  return arr;
 };
