00f06f74e3351fb294c37bcdda3bddbe12db0b6c
Sameer Jain <samir4nov@gmail.com>
2021-11-28 02:43:58 +0530

    actions and reducers for game mechanism

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,11 @@ export const COLOR = {
 };
 
 export const GAME_CARD_SIZE = 70;
+
+export const ACTION_TYPE = {
+  CREATE_GAME: 'CREATE_GAME',
+  CARD_CLICKED: 'CARD_CLICKED',
+  EVAL_CARD_CLICKED: 'EVAL_CARD_CLICKED',
+};
+
+export const MAX_CARD_MATCH = 2; // TODO: check if this works
---
+++ src/reducer.js
@@ -0,0 +1,70 @@
+import { shuffle } from './utils';
+import { ACTION_TYPE, MAX_CARD_MATCH } from './constants';
+
+export const initialState = {
+  matches: 0,
+  attempts: 0,
+  gameBoard: {},
+  openCardIndexes: [],
+};
+
+const initGameBoard = cards => {
+  const gameBoard = {};
+  const gameCards = shuffle([...cards, ...cards]); // Double the cards to 16, with 2 copies of each & Shuffle
+  gameCards.forEach((cardVal, index) => {
+    gameBoard[index] = {
+      value: cardVal,
+      show: false,
+      isDisabled: false,
+    };
+  });
+  return gameBoard;
+};
+
+const openCards = ({ gameBoard, openCardIndexes }, { clickedIndex }) => {
+  if (openCardIndexes.length >= MAX_CARD_MATCH) {
+    //TODO
+    return {};
+  }
+  console.log('OpenCards: ', clickedIndex);
+  return {
+    openCardIndexes: [...openCardIndexes, clickedIndex],
+    gameBoard: {
+      ...gameBoard,
+      [clickedIndex]: { ...gameBoard[clickedIndex], show: true },
+    },
+  };
+};
+
+const evaluateOpenCards = ({ gameBoard, openCardIndexes, attempts }) => {
+  return {
+    gameBoard: {
+      ...gameBoard,
+      [openCardIndexes[1]]: { ...gameBoard[openCardIndexes[1]], show: false }, // TODO: removed hardcode indexes
+      [openCardIndexes[0]]: { ...gameBoard[openCardIndexes[0]], show: false },
+    },
+    openCardIndexes: initialState.openCardIndexes,
+    attempts: attempts + 1,
+  };
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTION_TYPE.CREATE_GAME:
+      return { ...initialState, gameBoard: initGameBoard(action.payload) };
+    case ACTION_TYPE.CARD_CLICKED:
+      return {
+        ...state,
+        ...openCards(state, action.payload),
+      };
+    case ACTION_TYPE.EVAL_CARD_CLICKED:
+      return {
+        ...state,
+        ...evaluateOpenCards(state, action.payload),
+      };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
